69955ddcb60ed3ed7130fb95d7ff37c94343519a
Andreas Alexander <andreasalexander@Andreass-MacBook-Air.local>
2022-07-20 18:27:39 +0700

    insert data into database

--- src/create.ts
+++ src/create.ts
@@ -1,3 +1,30 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
+async function main() {
+  try {
+    const data = [
+      {name: 'Andreas Alexander', description: 'frontend tutorials', link: 'https://youtube.com/channel/andreas-alexander', subscribers: 100},
+      {name: 'Christopher Alexander', description: 'backend tutorials', link: 'https://youtube.com/channel/christopher-alexander', subscribers: 150},
+      {name: 'Harjanto Widodo', description: 'fullstack tutorials', link: 'https://youtube.com/channel/harjanto-widodo', subscribers: 200}
+    ];
+    
+    const res = await prisma.youtube_channels.createMany({
+      data,
+      skipDuplicates: true,
+    });
+
+    console.log(res);
+
+  } 
+  catch (error) {
+    console.log(error);
+  }
+  finally {
+    async () => {
+      await prisma.$disconnect();
+    }
+  }
+}
+
+main();
